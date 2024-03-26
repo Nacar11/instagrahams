4a39c8b28dd5bad81469da64591f40687b521d87
Bran Dale Nacario <nacariodale@gmail.com>
2024-03-26 14:59:01 +0800

    feat: configured laravel interia, vue
    feat: configured Layout File

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,10 @@
+<script setup>
+import MainLayout from "@/Layouts/MainLayout.vue";
+</script>
+
+<template>
+    <Head title="HOME" />
+    <MainLayout>
+        <!-- <div class="bg-green-500">Home.vue</div> -->
+    </MainLayout>
+</template>
